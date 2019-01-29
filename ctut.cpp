b6e8ad4a641cef18bb3bdcf2157add1ce0d7d213
hbj418 <hbj418@gmail.com>
2019-01-29 13:39:43 +0800

    hello wolrd

---
+++ ctut.cpp
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=Rub-JsjMhWY
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+	cout << "hello world" << endl;
+
+	return 0;
+}
